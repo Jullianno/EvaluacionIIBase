5a75f0aa539286c81bc7505f7e75ca007fb46f2d
20352526-5 <20352526-5@CAL_LAB11912>
2022-05-31 13:15:48 -0400

    Agregar archivos de proyecto.

---
+++ EvaluacionIIBase/Program.cs
@@ -0,0 +1,60 @@
+﻿
+
+const string nombreAlumno = "Wachimingo";
+
+while (Menu(nombreAlumno))
+{
+    Console.ReadKey(); // pausa, solicitar la entrada de una tecla
+}
+
+
+static bool Menu(string nombreAlumno)
+{ 
+    bool continuar = true;
+
+    Console.Clear(); // Limpia la pantalla
+    Console.Title = $"Evaluación II: {nombreAlumno}";
+
+    Console.WriteLine("Menú de opciones");
+    Console.WriteLine("================");
+    Console.WriteLine("1) Listar préstamos");
+    Console.WriteLine("2) Agregar préstamo");
+    Console.WriteLine("3) Actualizar préstamo");
+    Console.WriteLine("4) Eliminar préstamo");
+    Console.WriteLine("");
+    Console.WriteLine("0) Salir");
+
+    string opcion = Console.ReadLine().Trim(); // " 1 " => "1"
+
+    switch (opcion)
+    {
+        case "1":
+            Console.WriteLine("Listado de préstamos registrados");    
+           
+            break;
+        case "2":
+            Console.WriteLine("Insertar un nuevo préstamo");
+            
+            break;
+        case "3":
+            Console.WriteLine("Actualizar un préstamo existente");
+            
+            break;
+        case "4":
+            Console.WriteLine("Eliminar un préstamo existente");
+            
+            break;
+        case "0":
+            Console.WriteLine("Saliendo del programa ...");
+            continuar = false;
+            break;
+        default:
+            Console.WriteLine("Opción no válida");
+            break;
+    }
+
+    return continuar;
+}
+
+
+// ToDo: lógica de GUI
---
+++ LibreriaEvaluacion/DTO/PrestamoDTO.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaEvaluacion.DTO
+{
+    public class PrestamoDTO
+    {
+        // ToDo: atributos
+
+        private int id;
+        private int monto;
+        private int montoMasInteres;
+        private int montoAtraso;
+
+        // ToDo: propiedades
+
+        public int Id { get => id; set => id = value; }
+        public int Monto { get => monto; set => monto = value; }
+        public int MontoMasInteres { get => montoMasInteres; set => montoMasInteres = value; }
+        public int MontoAtraso { get => montoAtraso; set => montoAtraso = value; }
+
+        static List<PrestamoDTO> datos = new List<PrestamoDTO>()
+        {
+            new PrestamoDTO {id = 1, monto = 100000},
+            new PrestamoDTO {id = 2, monto = 150000},
+            new PrestamoDTO {id = 3, monto = 200000}
+        };
+
+
+        // ToDo: métodos
+        public static bool Add(PrestamoDTO nuevosDatos)
+        {
+            try
+            {
+                datos.Add(nuevosDatos); 
+
+                return true; 
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static bool RemoveAt(int indice)
+        {
+            try
+            {
+                datos.RemoveAt(indice);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+    }
+}
